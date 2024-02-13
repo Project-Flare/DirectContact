5425ce80e5c38d0334ae0d8ff9e2f77497087a00
herkusleonkaselis <herkus.kaselis@ktu.edu>
2024-02-13 21:13:28 +0200

    dev: restore non-ASP.NET server

--- DirectContactServer/Program.cs
+++ DirectContactServer/Program.cs
@@ -1,23 +1,10 @@
-using DirectContactServer.Services;
-
-namespace DirectContactServer
+﻿namespace DirectContactServer
 {
-    public class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddGrpc();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            app.MapGrpcService<GreeterService>();
-            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-            app.Run();
+            Console.WriteLine("Hello, World!");
         }
     }
 }
