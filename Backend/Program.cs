9e3e5094b83caf906e279fadbe59b7fd269e0de9
Manfredas Lamsargis <manfredas.lamsargis@proton.me>
2024-03-14 22:50:58 +0200

    dev: Created and configurated for .proto files Backend Console App project

---
+++ Backend/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Net.WebSockets;
+using Flare;
+using Google.Protobuf;
+using static Flare.ServerMessage;
+namespace Backend
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string ServerUrl = "wss://ws.project-flare.net/";
+            const int MaxServerResponseTimeSeconds = 5;
+
+            var serverUri = new Uri(ServerUrl);
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(MaxServerResponseTimeSeconds));
+
+            using (var webSocket = new ClientWebSocket())
+            {
+                try
+                {
+                    // Wait for connection to the server
+                    await webSocket.ConnectAsync(serverUri, cts.Token);
+
+                    // Get "Hello" message from the server
+                    byte[] buffer = new byte[1024];
+                    WebSocketReceiveResult serverResponse = await webSocket.ReceiveAsync(buffer, cts.Token);
+                    ServerMessage response = Parser.ParseFrom(buffer, 0, serverResponse.Count);
+
+                    if (response.ServerMessageTypeCase != ServerMessageTypeOneofCase.Hello)
+                    {
+                        Console.WriteLine("Failed to greet the server");
+                        return;
+                    }
+                    Console.WriteLine("Connected to the server successfully");
+
+                    // Attempt user registration
+                    Registration userRegistration = new Registration();
+                    
+                    // Max 10 times for setting username
+                    for (int times = 0; times < 10; times++)
+                    {
+                        Console.Write("Username: ");
+                        string? username = Console.ReadLine();
+                        if (username is null)
+                            return;
+                        bool usernameValid = userRegistration.UsernameValid(username);
+                        Console.WriteLine("Username is valid: " + userRegistration.TrySetUsername(username));
+                        if (usernameValid)
+                        {
+                            Console.WriteLine("Welcome, " + userRegistration.Username);
+                            break;
+                        }
+                    }
+
+                    // Max 20 tries to set correctly account password
+                    for (int times = 0; times < 10; times++)
+                    {
+                        Console.Write("Password: ");
+                        string? password = Console.ReadLine();
+                        if (password is null)
+                            return;
+                        bool passwordValid = userRegistration.TrySetPassword(password);
+                        if (passwordValid)
+                        {
+                            Console.WriteLine("Password: " + password + '\n' + userRegistration.EvaluatePassword(password));
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Password objected: " + password + '\n' + userRegistration.EvaluatePassword(password));
+                        }
+                    }
+
+                    RegisterRequest? request = userRegistration.FormRegistrationRequest();
+                    if (request is null)
+                    {
+                        Console.WriteLine("Forming registration request for server failed.");
+                    }
+                    else
+                    {
+                        // Send request for user registration
+                        byte[] bytes = request.ToByteArray();
+                        await webSocket.SendAsync(bytes, WebSocketMessageType.Binary, true, cts.Token);
+
+                        WebSocketReceiveResult registerResponse = await webSocket.ReceiveAsync(bytes, cts.Token);
+                        ServerMessage regResponse = Parser.ParseFrom(bytes, 0, serverResponse.Count);
+                        if (!response.ServerMessageTypeCase.Equals(ServerMessageTypeOneofCase.RegisterResponse))
+                        {
+                            Console.WriteLine("Registration failed...");
+                        }
+                        else
+                        {
+                            Console.WriteLine(userRegistration.Username + " your account is registered to Direct Contact!");
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Service terminated because of:\n" + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Backend/Registration.cs
@@ -0,0 +1,139 @@
+﻿using Flare;
+using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Zxcvbn;
+using static Flare.ServerMessage;
+
+namespace Backend
+{
+    public enum PasswordStrength
+    {
+        None, // interpret None as invalid password
+        Unacceptable,
+        Weak,
+        Good,
+        Excellent
+    }
+
+    internal class Registration
+    {
+        public string Username { get => username; }
+        public string Password { get => password; }
+        
+        private string username;
+        private string password;
+
+        public Registration()
+        {
+            username = string.Empty;
+            password = string.Empty;
+        }
+
+        public bool TrySetPassword(string password)
+        {
+            var evaluation = EvaluatePassword(password);
+            switch (evaluation)
+            {
+                case PasswordStrength.None:
+                    return false;
+                case PasswordStrength.Unacceptable:
+                    return false;
+                case PasswordStrength.Weak:
+                    return false;
+                case PasswordStrength.Good:
+                    this.password = password;
+                    return true;
+                case PasswordStrength.Excellent:
+                    this.password = password;
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public PasswordStrength EvaluatePassword(string password)
+        {
+            // just invalid input
+            if (string.IsNullOrEmpty(password) || string.IsNullOrWhiteSpace(password))
+                return PasswordStrength.None;
+
+            // Entropy is estimated using Dan Wheeler's zxcvbn algorithm.
+            int entropy = (int)Math.Log2(
+                Core.EvaluatePassword(password).Guesses
+                );
+
+            // [0;50] - unacceptable
+            if (entropy <= 50)
+                return PasswordStrength.Unacceptable;
+
+            // (50; 70] - weak
+            if (entropy <= 70)
+                return PasswordStrength.Weak;
+
+            // (70, 90] - good
+            if (entropy <= 90)
+                return PasswordStrength.Good;
+
+            return PasswordStrength.Excellent;
+        }
+
+        public bool TrySetUsername(string username)
+        {
+            if (!UsernameValid(username))
+                return false;
+
+            this.username = username;
+            return true;
+        }
+
+        public bool UsernameValid(string username)
+        {
+            // Just to be safe
+            if (string.IsNullOrEmpty(username) || string.IsNullOrWhiteSpace(username))
+                return false;
+
+            // Username must contain only ASCII characters
+            if (!ContainsOnlyAscii(username))
+                return false;
+
+            // Must contain alphanumerical symbols only
+            Regex regex = new Regex(@"^[\d\w]{1,32}$", RegexOptions.IgnoreCase);
+            if (!regex.IsMatch(username))
+                return false;
+
+            return true;
+        }
+
+        public bool ContainsOnlyAscii(string str)
+        {
+            // C# moment
+            foreach (char c in str)
+                if (!char.IsAscii(c))
+                    return false;
+            return true;
+        }
+
+        public RegisterRequest? FormRegistrationRequest()
+        {
+            // Don't send request if username or password is not set
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                return null;
+
+            // Form register request protobuf
+            RegisterRequest request = new RegisterRequest
+            {
+                Username = username,
+                Password = password
+            };
+
+            return request;
+        }
+    }
+}
